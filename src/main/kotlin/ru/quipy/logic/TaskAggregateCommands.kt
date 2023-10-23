dcd794493492df600100c200b8e752a93d905630
gravitacia <kocagg@mail.ru>
2023-10-23 17:33:03 +0300

    feat : add aggregate state

--- src/main/kotlin/ru/quipy/logic/TaskAggregateCommands.kt
+++ src/main/kotlin/ru/quipy/logic/TaskAggregateCommands.kt
@@ -1,2 +1,43 @@
 package ru.quipy.logic
 
+import ru.quipy.api.task.*
+import ru.quipy.api.task.TaskCreatedEvent
+import java.util.*
+
+
+fun TaskAggregateState.create(id: UUID, title: String, creatorId: String, projectId: UUID): TaskCreatedEvent {
+    return TaskCreatedEvent(
+            taskId = id,
+            title = title,
+            projectId = projectId
+    )
+}
+
+fun TaskAggregateState.changeTitle(id: UUID, title: String): TaskTitleChangedEvent {
+    return TaskTitleChangedEvent(
+            taskId = id,
+            title = title
+    )
+}
+
+fun TaskAggregateState.assignStatus(id: UUID, statusId: UUID): StatusAssignedToTaskEvent {
+    return StatusAssignedToTaskEvent(
+            taskId = id,
+            statusId = statusId,
+    )
+}
+
+fun TaskAggregateState.addExecutor(id: UUID): TaskAddedExecutorEvent {
+    if (assignees.values.any { it.userId == id }) {
+        throw IllegalArgumentException("User already exists: $id")
+    }
+    return TaskAddedExecutorEvent(taskId = this.getId(), executorId = id)
+}
+
+fun TaskAggregateState.removeExecutor(id: UUID): TaskRemovedExecutor {
+    if (!assignees.values.any { it.userId == id }) {
+        throw IllegalArgumentException("User doesn't exists: $id")
+    }
+
+    return TaskRemovedExecutor(taskId = this.getId(), executorId = id)
+}
--- src/main/kotlin/ru/quipy/logic/TaskAggregateState.kt
+++ src/main/kotlin/ru/quipy/logic/TaskAggregateState.kt
@@ -1,2 +1,58 @@
 package ru.quipy.logic
 
+import ru.quipy.api.task.*
+import ru.quipy.api.task.TaskCreatedEvent
+import ru.quipy.core.annotations.StateTransitionFunc
+import ru.quipy.domain.AggregateState
+import java.util.*
+
+class TaskAggregateState : AggregateState<UUID, TaskAggregate> {
+    private lateinit var taskId: UUID
+    var createdAt: Long = System.currentTimeMillis()
+    var updatedAt: Long = System.currentTimeMillis()
+    lateinit var projectId: UUID
+    lateinit var statusId: UUID
+    lateinit var taskTitle: String
+    var assignees = mutableMapOf<UUID, UserEntityTask>()
+
+    override fun getId() = taskId
+
+    @StateTransitionFunc
+    fun taskCreatedApply(event: TaskCreatedEvent) {
+        projectId = event.projectId
+        taskTitle = event.title
+        updatedAt = createdAt
+        taskId = event.taskId
+    }
+
+    @StateTransitionFunc
+    fun taskTitleChangedApply(event: TaskTitleChangedEvent) {
+        taskTitle = event.title
+        updatedAt = createdAt
+        taskId = event.taskId
+    }
+
+    @StateTransitionFunc
+    fun userExecutorApply(event: TaskAddedExecutorEvent) {
+        taskId = event.taskId
+        assignees[event.executorId] = UserEntityTask(event.executorId)
+        updatedAt = createdAt
+    }
+
+    @StateTransitionFunc
+    fun taskStatusAssignedApply(event: StatusAssignedToTaskEvent) {
+        taskId = event.taskId
+        updatedAt = event.createdAt
+        statusId = event.statusId
+    }
+
+    @StateTransitionFunc
+    fun removeExecutor(event: TaskRemovedExecutor) {
+        taskId = event.taskId
+        assignees.remove(event.executorId)
+    }
+}
+
+data class UserEntityTask(
+        val userId: UUID
+)
